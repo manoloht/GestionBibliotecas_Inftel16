4d7ff6a567cfb885d5f3d2b4a5866c73be9f9938
GROUP\YUEMEI <yuemeijiang1991@hotmail.com>
2016-12-02 12:41:59 +0100

    otro println yuemei

--- GestionBibliotecas/src/gestionbibliotecas/GestionBibliotecas.java
+++ GestionBibliotecas/src/gestionbibliotecas/GestionBibliotecas.java
@@ -17,6 +17,7 @@ public class GestionBibliotecas {
     public static void main(String[] args) {
         // TODO code application logic here
         //yuemei
+        System.out.println("yuemei");
         
         
     }
