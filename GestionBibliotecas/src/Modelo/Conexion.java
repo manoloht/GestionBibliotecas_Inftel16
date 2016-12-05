b438038d3aab0b7fea02b419e0d7dddab1801d00
inftel07 <inftel07@192.168.183.58>
2016-12-05 14:04:10 +0100

    Añadido Fichero Properties

--- GestionBibliotecas/src/Modelo/Conexion.java
+++ GestionBibliotecas/src/Modelo/Conexion.java
@@ -5,24 +5,24 @@
  */
 package Modelo;
 
-import java.io.InputStream;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 /**
  *
  * @author Manuel
  */
-
 public class Conexion {
-    
-    public Conexion(){
+
+    public Conexion() {
     }
-    
-    public Connection getConnection() throws Exception{
+
+    /*public Connection getConnection() throws Exception{
         Properties pr = new Properties();
-        InputStream config = ClassLoader.getSystemResourceAsStream(Conexion.class.getPackage().getName()+"/bd.properties");
+        InputStream config = ClassLoader.getSystemResourceAsStream(Conexion.class.getPackage().getName()+"/bd.properties");    
         pr.load(config);
         config.close();
         
@@ -38,11 +38,35 @@ public class Conexion {
         Class.forName(driver);
         
         // Preparamos la conexion
-        String cadConexion=subprotocolo+":"+usuario+"/"+password+"@"+url+":"+puerto+":"+sid;
+        String cadConexion = subprotocolo+":"+usuario+"/"+password+"@"+url+":"+puerto+":"+sid;
         
         // Obtenemos la conexion
         Connection con = DriverManager.getConnection(cadConexion);
         return con;
         
-        }
+        }*/
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+        Properties pr = new Properties();
+        InputStream config = ClassLoader.getSystemResourceAsStream(Conexion.class.getPackage().getName() + "/bd.properties");
+        pr.load(config);
+        config.close();
+
+        String driver = pr.getProperty("driver");
+        String subprotocolo = pr.getProperty("subprotocolo");
+        String usuario = pr.getProperty("usuario");
+        String password = pr.getProperty("password");
+        String url = pr.getProperty("url");
+        int puerto = Integer.parseInt(pr.getProperty("puerto"));
+        String sid = pr.getProperty("sid");
+
+        // Cargamos el driver
+        Class.forName(driver);
+
+        // Preparamos la conexion
+        String cadConexion = subprotocolo + ":" + usuario + "/" + password + "@" + url + ":" + puerto + ":" + sid;
+
+        // Obtenemos la conexion
+        Connection con = DriverManager.getConnection(cadConexion);
+    }
+
 }
